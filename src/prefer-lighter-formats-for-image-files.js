1ac63fdb4289e8da8e96c2a6178c5993b04d5d95
Anthony O. <anthony-o@users.noreply.github.com>
2025-02-03 12:28:43 +0100

    Add rule GCI31 example (#14)
    Co-authored-by: Lucas Leroux <leroux.lucas@hotmail.com>
    Co-authored-by: Vincent-Letourmy <letourmyvincent@outlook.fr>
    Co-authored-by: utarwyn <utarwyn@users.noreply.github.com>

---
+++ src/prefer-lighter-formats-for-image-files.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function ImageFilesComponent() {
+  return (
+    <div>
+      {/* Compliant: WEBP */}
+      <img src="./assets/images/cat.webp" alt="A cat" />
+
+      {/* Compliant: AVIF */}
+      <img src="./assets/images/cat.avif" alt="A cat" />
+
+      {/* Compliant: JPEG XL */}
+      <img src="./assets/images/cat.jxl" alt="A cat" />
+
+      {/* Compliant: JPG inside a picture */}
+      <picture>
+        <source srcSet="image.webp" type="image/webp" />
+        <img src="image.jpg" alt="..." />
+      </picture>
+
+      {/* Compliant: not handled */}
+      <img src="./assets/images/cat" alt="A cat" />
+
+      {/* Non-compliant: JPG*/}
+      <img src="./assets/images/cat.jpg" alt="A cat" />
+
+      {/* Non-compliant: PNG*/}
+      <img src="./assets/images/cat.png" alt="A cat" />
+    </div>
+  );
+}
+
+export default ImageFilesComponent;
