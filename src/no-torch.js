1fcdd43dcee5419c28271f69a3f7e63e2ae5bb2b
utarwyn <utarwyn@users.noreply.github.com>
2025-04-22 00:01:55 +0200

    Move to ESLint v9

--- src/no-torch.js
+++ src/no-torch.js
@@ -1,1 +1,3 @@
-import Torch from 'react-native-torch'; // Non-compliant: torch should not be enabled
+import Torch from "react-native-torch"; // Non-compliant: torch should not be enabled
+
+Torch.switchState(true);
